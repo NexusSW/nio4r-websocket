d80725a33af47fb06fb88e0e6c308c650e2cdddd
Sean Zachariasen <thewyzard@hotmail.com>
2017-10-03 20:40:07 -0600

    enable rake spec

--- spec/nio4r/websocket_spec.rb
+++ spec/nio4r/websocket_spec.rb
@@ -1,11 +1,1 @@
 require 'spec_helper'
-
-RSpec.describe Nio4r::Websocket do
-  it 'has a version number' do
-    expect(Nio4r::Websocket::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
-require 'bundler/setup'
-require 'nio4r/websocket'
+require 'nio/websocket'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
